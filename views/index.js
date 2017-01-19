f91431e64e214744040353c112283d4ed92b58a4
Parthjhaveri <parthjhaveri209@gmail.com>
2017-01-19 12:39:29 -0500

    final backend

--- views/index.js
+++ views/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import SchoolComponent from '../components/schoolCom.jsx'
-// import StudentComponent from '../components/studentCom.jsx'
 
 var App = React.createClass({
 
 	render() {
 		return (
 			<div>
-				<h1>Yelp!</h1>
+				<h1>Yelp Final!</h1>
 			</div>
 		)
 	}
